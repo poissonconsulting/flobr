8db08fc31a48278854df4d95325634e871bccec0
joethorley <joe@poissonconsulting.ca>
2019-07-25 09:22:42 -0700

    test with csv
    slight difference pdf on windows

--- tests/testthat/test-flobr.R
+++ tests/testthat/test-flobr.R
@@ -22,7 +22,7 @@ test_that("flob_noname", {
   expect_error(unflob(flob_noname, tempfile(fileext = ".pdf1")), "path extension must match 'pdf'")
 })
 
-test_that("package", {
+test_that("package with pdf", {
 
   path <- system.file("extdata", "flobr.pdf", package = "flobr", mustWork = TRUE)
 
@@ -36,8 +36,45 @@ test_that("package", {
   expect_identical(flob_name(flob), "flobr")
 
   new_path <- unflob(flob, file.path(dirname(tempfile()), ""))
-  expect_equivalent(flob(new_path), flob)
   expect_error(unflob(flob, tempfile(fileext = ".pdf1")), "path extension must match 'pdf'")
   file <- tempfile()
   expect_identical(unflob(flob, file), paste0(file, ".pdf"))
+
+  flob2 <- flob(new_path)
+  expect_identical(flob_name(flob2), flob_name(flob))
+  expect_identical(flob_ext(flob2), flob_ext(flob))
+  expect_identical(flob2[[1]][1:133838], flob[[1]][1:133838])
+
+  # on windows there is a slight difference in the flobs to the end
+  # flob: 09 00 00 00 | 09 66 6c 6f 62 72 | 2e 70 64 66 00 00 00 fe 00 00 00 00 00 00
+  # flob2: 09 00 00 00 | 0e 52 74 6d 70 53 77 6f 52 4f 36 | 2e 70 64 66 00 00 00 fe 00 00 00 00 00 00
+  # the files appears to be the same
+  skip_on_os("windows")
+  expect_equivalent(flob(new_path), flob)
+})
+
+test_that("package with csv", {
+
+  path <- system.file("extdata", "count.csv", package = "flobr", mustWork = TRUE)
+
+  expect_error(flob(1), "path must be class character")
+  expect_error(flob(paste(path, "1")), "csv 1' does not exist")
+  flob <- flob(path)
+  expect_true(is_flob(flob))
+  check_flob(flob)
+  expect_identical(names(flob), path)
+  expect_identical(flob_ext(flob), "csv")
+  expect_identical(flob_name(flob), "count")
+
+  new_path <- unflob(flob, file.path(dirname(tempfile()), ""))
+  expect_error(unflob(flob, tempfile(fileext = ".pdf1")), "path extension must match 'csv'")
+  file <- tempfile()
+  expect_identical(unflob(flob, file), paste0(file, ".csv"))
+
+  flob2 <- flob(new_path)
+  expect_identical(flob_name(flob2), flob_name(flob))
+  expect_identical(flob_ext(flob2), flob_ext(flob))
+
+  expect_identical(flob2[[1]], flob[[1]])
+  expect_equivalent(flob(new_path), flob)
 })
