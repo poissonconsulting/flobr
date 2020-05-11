e691b5a192c6042cdaa9458033226abf58337eaf
Joe Thorley <joe@poissonconsulting.ca>
2020-05-11 13:39:18 -0700

    getting ready for cran release

--- build.R
+++
@@ -1,9 +0,0 @@
-styler::style_pkg()
-devtools::test()
-devtools::document()
-# knitr::knit("README.Rmd")
-if(FALSE) {
-  if(file.exists("DESCRIPTION")) unlink("docs", recursive = TRUE)
-  pkgdown::build_site()
-}
-devtools::check()
