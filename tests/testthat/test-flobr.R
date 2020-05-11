900d35a6cced0d620dcd96506980efb9498d61da
Joe Thorley <joe@poissonconsulting.ca>
2020-05-11 13:46:26 -0700

    lint

--- tests/testthat/test-flobr.R
+++ tests/testthat/test-flobr.R
@@ -30,7 +30,8 @@ test_that("flob_noname", {
 })
 
 test_that("package with pdf", {
-  path <- system.file("extdata", "flobr.pdf", package = "flobr", mustWork = TRUE)
+  path <- system.file("extdata", "flobr.pdf", package = "flobr",
+                      mustWork = TRUE)
 
   expect_error(flob(1),
     "^`path` must be a string [(]non-missing character scalar[)][.]$",
@@ -38,7 +39,8 @@ test_that("package with pdf", {
   )
   expect_error(
     flob(paste(path, "1")),
-    "^`path` must specify an existing file [(]'.*[.]pdf 1' can't be found[)][.]",
+    p0("^`path` must specify an existing file [(]'.*[.]pdf 1' can't ",
+       "be found[)][.]"),
     class = "chk_error"
   )
   flob <- flob(path)
