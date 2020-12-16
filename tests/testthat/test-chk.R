3a62d365d58bb564f3b7087158de17b9ef68e640
Evan <evanamiesgalonski@gmail.com>
2020-12-16 11:37:27 -0800

    test chk_blob

--- tests/testthat/test-chk.R
+++ tests/testthat/test-chk.R
@@ -21,3 +21,30 @@ test_that("chk_flob", {
     class = "chk_error"
   )
 })
+
+test_that("chk_blob", {
+  expect_null(chk_blob(blob_obj))
+  expect_invisible(chk_blob(blob_obj))
+
+  expect_null(chk_blob(flob_obj))
+  expect_invisible(chk_blob(flob_obj))
+
+  expect_null(chk_blob(flob_old))
+  expect_invisible(chk_blob(flob_old))
+
+
+  expect_error(chk_blob(1), "^`1` must inherit from S3 class 'blob'[.]$",
+               class = "chk_error"
+  )
+  x <- as_blob(charToRaw("oeu"))
+  expect_error(chk_blob(x), "^`x` must be a blob of a serialized object[.]$",
+               class = "chk_error"
+  )
+
+  x <- list(x = 1)
+  class(x) <- "blob"
+  expect_error(chk_blob(x), "^`x` must be a blob of a serialized object[.]$",
+               class = "chk_error"
+  )
+})
+
