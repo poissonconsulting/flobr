64c8659075f1728aae72ceb2e9274ead753a3747
Evan <evanamiesgalonski@gmail.com>
2020-12-16 10:42:57 -0800

    make test blob internal

--- data-raw/blob-obj.R
+++ data-raw/blob-obj.R
@@ -4,4 +4,4 @@ blob_obj <- readBin(file, what = "integer", n = n, endian = "little") %>% list()
 names(blob_obj) <- "name"
 blob_obj %<>% serialize(NULL) %>% list() %>% blob::as_blob()
 
-usethis::use_data(blob_obj, overwrite = TRUE)
+usethis::use_data(blob_obj, overwrite = TRUE, internal = TRUE)
