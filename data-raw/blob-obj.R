9dd54b5ddf3d855cda80a7509dcbefa2600da9d3
Evan <evanamiesgalonski@gmail.com>
2020-12-16 10:30:42 -0800

    add blob object to data raw for testing

---
+++ data-raw/blob-obj.R
@@ -0,0 +1,7 @@
+file <- "inst/extdata/flobr.pdf"
+n <- file.info(file)$size
+blob_obj <- readBin(file, what = "integer", n = n, endian = "little") %>% list()
+names(blob_obj) <- "name"
+blob_obj %<>% serialize(NULL) %>% list() %>% blob::as_blob()
+
+usethis::use_data(blob_obj, overwrite = TRUE)
